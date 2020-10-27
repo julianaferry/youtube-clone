f27d43eb8a45b75b15320935500d27c8cecc2471
Juliana <julianafery@gmail.com>
2020-10-27 10:02:21 +0000

    files added

--- src/api/youtubeapi.js
+++ src/api/youtubeapi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const KEY = '';
+export const KEY = 'AIzaSyAT19ykoV0Of3Nrk5TTS_kzYX6dDk5zq90';
 
 export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3',
+    baseURL: 'https://www.googleapis.com/youtube/v3'
 })
